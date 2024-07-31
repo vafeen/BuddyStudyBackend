ffaa5827d1bac355540d136c889cd519a7525ba4
iaroslav_shavrov <iaroslav_shavrov@practice.relex.ru>
2024-07-31 04:32:00 +0300

    настройка

--- client/src/common/styleConstants.ts
+++ client/src/common/styleConstants.ts
@@ -1,6 +1,7 @@
 export const colors = {
-    greyAccent: "rgba(235, 235, 235, 1)",
-    main: "rgba(122, 122, 122, 1)",
+    grayAccent: "rgba(235, 235, 235, 1)",
+    grayDark: "rgba(122, 122, 122, 1)",
+    main: "rgba(0, 0, 0, 1)",
     totalBlack: "rgba(0, 0, 0, 1)",
     totalWhite: "rgba(255, 255, 255, 1)",
 }
