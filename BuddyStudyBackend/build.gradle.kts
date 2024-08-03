746a2ef4dc5c01b9010c10ea556c1fc0db0a7ee6
Vafeen <666av6@gmail.com>
2024-08-04 01:26:41 +0300

    Add ktor implementation

--- BuddyStudyBackend/build.gradle.kts
+++ BuddyStudyBackend/build.gradle.kts
@@ -8,9 +8,12 @@ version = "1.0"
 repositories {
     mavenCentral()
 }
+val ktor_version: String by project
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
 }
 
 tasks.test {
