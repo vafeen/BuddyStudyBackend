b4845916c39f13f574b48ceba9eacad00f4ce750
Vafeen <666av6@gmail.com>
2024-08-04 01:21:24 +0300

    Initial backend commit

---
+++ BuddyStudyBackend/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "ru.vafeen"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ BuddyStudyBackend/src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package ru.vafeen
+
+fun main() {
+    println("Hello World!")
+}
