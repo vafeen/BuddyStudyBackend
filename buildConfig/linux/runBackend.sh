c7efe4b7a77f0d7cedbbe73bfe3d2941414b9623
Vafeen <666av6@gmail.com>
2024-08-12 06:08:53 +0300

    update runBakckend config file

--- buildConfig/linux/runBackend.sh
+++ buildConfig/linux/runBackend.sh
@@ -4,4 +4,4 @@ bash ./buildBackend.sh
 cd ../../backend
 ./gradlew run
 cd ./backend/build/install
-bash ./backend/build/install/BuddyStudyBackend/bin//BuddyStudyBackend
+bash ./backend/build/install/BuddyStudyBackend/bin/BuddyStudyBackend
