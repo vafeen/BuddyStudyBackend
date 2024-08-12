ee08048aa2c5bc0860235263a74d69d6daef1391
Vafeen <666av6@gmail.com>
2024-08-12 06:00:50 +0300

    Remove test implementation

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -14,17 +14,13 @@ val ktor_version = "2.3.12"
 val logback_version = "1.5.6"
 dependencies {
 
-    testImplementation(kotlin("test"))
-//    implementation("io.ktor:ktor-client-core:$ktor_version")
-//    implementation("io.ktor:ktor-client-cio:$ktor_version")
+
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$ktor_version")
 
     //db
     implementation("io.ktor:ktor-server-core:2.3.0")
